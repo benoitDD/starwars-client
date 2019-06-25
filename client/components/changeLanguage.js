21753e07fe162861b0e1d598b2a8a57a076aadb7
benoitDD <benoitdegorsas@outlook.fr>
2019-06-25 18:50:07 +0200

    spritesheet flags

--- client/components/changeLanguage.js
+++ client/components/changeLanguage.js
@@ -1,8 +1,5 @@
 import React, {Component} from 'react'
 import {withTranslation} from 'react-i18next'
-import es from '../images/es.png'
-import en from '../images/en.png'
-import fr from '../images/fr.png'
 import './changeLanguage.sass'
 import Dropdown from './utils/dropdown'
 import PropTypes from 'prop-types'
@@ -10,17 +7,14 @@ import PropTypes from 'prop-types'
 const languages = [
 	{
 		code: 'es',
-		image: es,
 		label: 'spanish'
 	},
 	{
 		code: 'en',
-		image: en,
 		label: 'english'
 	},
 	{
 		code: 'fr',
-		image: fr,
 		label: 'french'
 	}
 ]
@@ -48,14 +42,14 @@ class ChangeLanguage extends Component {
 						<div className = 'changeLanguage_langage'
 							onClick = {() => this.props.i18n.changeLanguage(language.code)}
 						>
-							<img src = {language.image} alt = {language.code}/>
+							<div className = {`flag ${language.code}`}/>
 							<span className = 'changeLanguage_label'>{this.props.t(language.label)}</span>
 						</div>
 					)}
                 
 					renderHeader = {language => (
 						<div className = 'changeLanguage_langage'>
-							<img src = {language.image} alt = {language.code}/>
+							<div className = {`flag ${language.code}`}/>
 							<span className = 'changeLanguage_label'>{this.props.t(language.label)}</span>
 						</div>
 					)}
